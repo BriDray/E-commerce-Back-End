2f7c44cbfb07824ed949336660ae4f9b617227a9
Bri Dray <stanggurl02@gmail.com>
2023-05-17 13:44:58 -0700

    added tag code

---
+++ Develop/models/Tag.js
@@ -0,0 +1,31 @@
+// importing sequelize library
+const { Model, DataTypes } = require('sequelize');
+// importing database
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+// product tag info
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+// exporting tag info
+module.exports = Tag;
